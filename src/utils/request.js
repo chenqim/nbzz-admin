fe02143f50737ca1ac8f7d0e74c9b8f2027d063b
chenqiming <chenqiming@jiagouyun.com>
2025-08-18 00:56:09 +0800

    增加登录过期

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
+import router from '@/router'
 import { getToken } from '@/utils/auth'
 
 // create an axios instance
@@ -95,6 +96,9 @@ const handleError = (code, message) => {
     offset: '80',
     duration: 5 * 1000
   })
+  if (code === 'A0230') {
+    window.location.reload()
+  }
 }
 
 export default service
